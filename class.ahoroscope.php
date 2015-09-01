b6dbf7fa3b44d0bd6b7f9eaf2e58affa8e126a3b
sdeemer2015 <sd@ynot.us>
2015-09-01 14:12:20 -0400

    Update class.ahoroscope.php

--- class.ahoroscope.php
+++ class.ahoroscope.php
@@ -1,56 +1,365 @@
- <?php
-// class.ahoroscope.php
-// version 1.0.0, 6 August, 2005
-//
-// Ahoroscope is a small class to load the webpage from http://www.astrology.com/
-// for the given astrology sign, then extract the horoscope and return
-// it as a string. The following are the twelve possible values for the $sign.
-// capricorn
-// aquarius
-// pisces
-// aries
-// taurus
-// gemini
-// cancer
-// leo
-// virgo
-// libra
-// scorpio
-// sagittarius
-//
-// License
-//
-// Copyright (C) 2005 George A. Clarke, webmaster@gaclarke.com, http://gaclarke.com/
-//
-// This program is free software; you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free Software
-// Foundation; either version 2 of the License, or (at your option) any later
-// version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-// Place - Suite 330, Boston, MA 02111-1307, USA.
-//
-
+<?php
 class ahoroscope
 {
- function getHoroscope($sign)
-{
- $lnk="http://horoscopes.astrology.com/dailylong".$sign.".html";
- $pg=file_get_contents($lnk);
- if(strpos($pg,"iTxt")!==false)
- {
-  $ii=strpos($pg,"iTxt")+6;
-  $ij=strpos($pg,'<',$ii);
-  $hor=substr($pg,$ii,$ij-$ii);
- }else{
-  $hor="Not found";
- }
-return $hor;
+	function getHoroscope_daily($sign)
+	{       
+		$lnk="http://my.horoscope.com/astrology/free-daily-horoscope-".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg, 'class="fontdef1" style="padding-right:10px;" id="textline"')!==false)
+		{
+			$aa=strpos($pg,'class="fontdef1" style="padding-right:10px;" id="textline"')+59;
+			$ab=strpos($pg,'</div>',$aa);
+			$hor="<div class='date'>".substr($pg,$aa,$ab-$aa)."</div>";
+			/*
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'</div>',$ii);
+			$hor.="<div class='mytext'>".substr($pg,$ii,$ij-$ii)."</div>";
+*/
+			
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+//	
+	
+	function getHoroscope_yesterday($sign)
+	{       
+		$lnk="http://my.horoscope.com/astrology/yesterday-horoscope-".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg, 'id="advert"')!==false)
+		{
+			$aa=strpos($pg,'id="advert"')+12;
+			$ab=strpos($pg,'</div>',$aa);
+			$hor="<div class='date'>".substr($pg,$aa,$ab-$aa)."</div>";
+			
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'</div>',$ii);
+			$hor.="<div class='mytext'>".substr($pg,$ii,$ij-$ii)."</div>";
+
+			
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+	
+	
+	function getHoroscope_weekly($sign)
+	{
+		$lnk="http://my.horoscope.com/astrology/free-weekly-horoscope-".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_monthly($sign)
+	{
+		$lnk="http://my.horoscope.com/astrology/free-monthly-horoscope-".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+	
+	
+	function getHoroscope_teen($sign)
+	{
+		$lnk="http://astrology.horoscope.com/free-weekly-teen-horoscope-".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'</div',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+	
+	function getHoroscope_romance($sign)
+	{
+		$lnk="http://love.horoscope.com/love/daily-love-horoscope-".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	function getHoroscope_single($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailysingles".$sign.".html";
+		//echo $lnk;
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,"textline")!==false)
+		{
+			$ii=strpos($pg,"textline")+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_couple($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailyrom".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+	
+	function getHoroscope_wromance($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/weeklyrom".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_career($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/monthlycar".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_fitness($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/monthlyfit".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+	function getHoroscope_tech($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailytech".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_flirt($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailyfunflirty".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_beauty($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailybeauty".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_slam($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailyslam".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_baby($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailybabyscope".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_cat($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailycatscope".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_dog($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailydogscope".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_h_g($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/dailyhomeandgarden".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	
+	
+	function getHoroscope_bus($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/weeklybus".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_travel($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/weeklytravel".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_wflirt($sign)
+	{
+		$lnk="http://horoscopes.astrology.com/weeklyfunflirty".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'id="textline"')!==false)
+		{
+			$ii=strpos($pg,'id="textline"')+14;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
+	function getHoroscope_year($sign)
+	{
+		$lnk="http://www.astrology.com/year/overview/".$sign.".html";
+		$pg=file_get_contents($lnk);
+		if(strpos($pg,'OVERVIEW:</b>')!==false)
+		{
+			$ii=strpos($pg,'OVERVIEW:</b>')+13;
+			$ij=strpos($pg,'<',$ii);
+			$hor=substr($pg,$ii,$ij-$ii);
+		}else{
+			$hor="Not found";
+		}
+		return $hor;
+	}
 }
-}
-?> 
+?>
